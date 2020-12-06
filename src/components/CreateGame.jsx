a86545124af0db9e2a3cdc135d68f2aa56003a2b
Weily Lang <Weily@Weilys-MacBook-Air.local>
2020-12-06 15:16:48 -0500

    resolving multi commit conflicts

--- src/components/CreateGame.jsx
+++ src/components/CreateGame.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-<<<<<<< HEAD
 import {
     BrowserRouter as Router,
     Link
   } from "react-router-dom";
-=======
 import Firebase, {FirebaseContext} from '../services/firebase';
 
->>>>>>> ad75e211e8d37f39f11aa8a9589b9621d2b48dc4
 
 class CreateGame extends React.Component {
 
     render() {
         return(
-<<<<<<< HEAD
             <Link to='/room'> 
             <Button>Create a Game</Button>
             </Link>
-=======
-            <Button> Create a Game </Button>
->>>>>>> ad75e211e8d37f39f11aa8a9589b9621d2b48dc4
         )
     }
 }
--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -3,11 +3,8 @@ import CreateGame from '../components/CreateGame';
 import Header from '../components/Header';
 import JoinRoom from '../components/JoinRoom';
 import Ycard from '../components/Ycard';
-<<<<<<< HEAD
 import Xcard from '../components/Xcard';
-=======
 import SomeComponent from '../components/test'
->>>>>>> ad75e211e8d37f39f11aa8a9589b9621d2b48dc4
 
 
 class Welcome extends React.Component {
